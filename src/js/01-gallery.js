12793f85d335806620696c6ca5c9aae2cb4ebc51
SerhiySBV <serhiybelyavskii@gmail.com>
2022-11-07 16:10:04 +0200

    Task_2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,7 +4,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
 const galleryEl = document.querySelector('.gallery');
 
 function createMarckUp() {
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,16 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const BOX = 'videoplayer-current-time';
 
-console.log(Player);
+const playerEl = document.querySelector('#vimeo-player');
+
+const onPlay = function (data) {
+  const serializedState = JSON.stringify(data.seconds);
+  localStorage.setItem(BOX, serializedState);
+};
+const player = new Player(playerEl);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+if (localStorage.getItem(BOX)) {
+  player.setCurrentTime(Number(localStorage.getItem(BOX)));
+}
