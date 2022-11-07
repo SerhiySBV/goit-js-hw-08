be3bbc0a713018d922cb8cd3d139d034ed5f90bc
SerhiySBV <serhiybelyavskii@gmail.com>
2022-11-07 13:47:20 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function createMarckUp() {
+  return galleryItems
+    .map(
+      ({ original, preview, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+     alt=${description}
+    />
+  </a>
+</li>`
+    )
+    .join('');
+}
+
+galleryEl.insertAdjacentHTML('afterbegin', createMarckUp());
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,3 @@
-
+import Player from '@vimeo/player';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(Player);
